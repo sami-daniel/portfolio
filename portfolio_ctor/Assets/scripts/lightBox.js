cb34f3bb162cb8dcff223bae3ce1b7f16af196a7
Sami Daniel <samidaniel125@gmail.com>
2023-09-16 17:58:09 -0300

    Segunda secao 'hard skill' finalizada temporariamente.

--- portfolio_ctor/Assets/scripts/lightBox.js
+++ portfolio_ctor/Assets/scripts/lightBox.js
@@ -1,14 +1,24 @@
 const newMainScreen = document.querySelector('._lightbox')
 const overlay = document.querySelector('.overlay')
-const starter = document.querySelector('._start_light_box')
+const starter = document.querySelector('._starter_lgtBox_hard')
 const body = document.querySelector('body')
 const btnClose = document.querySelector('.button_close')
-let imgLightBox = document.querySelector('._lightbox>div>div:first-child>img');
+const imgsCarousselBootstrap = document.querySelectorAll('.img1BoststrappCarousel , .img2BoststrappCarousel , .img3BoststrappCarousel')
+let h1_1 = document.querySelector('.titleProject_Ligther')
+let h1_1_span = document.querySelector('.lighter_span_la')
+let description_p = document.querySelector('._lightbox>div:last-child p')
+
 starter.addEventListener('click', () => {
     overlay.classList.add('entry_overlay')
     body.style.overflowY = 'hidden'
     newMainScreen.classList.add('entry_ligth_configs')
-    imgLightBox.src = 'Assets/imgs/logos/logo_me.png'
+    const imgsLinks = [
+        'Assets/imgs/imgs/start_screen.png',
+        'Assets/imgs/imgs/win_screen.png',
+        'Assets/imgs/imgs/lose_screen.png'
+    ]
+    StartLightBox('BLACK JACK', 'C# .NET FRAMEWORK', 'rgb(207, 1, 207)', 'O jogo de 21, também conhecido como blackjack, é um dos jogos de cartas mais populares em cassinos e' +
+        'também pode ser jogado em casa ou online. O objetivo do jogo é ter uma mão com um valor total de 21 pontos, ou o mais próximo possível deste número, sem ultrapassá-lo. Mais em ', 'gihub.com/sami-daniel', 'https://github.com/sami-daniel/sami-daniel', imgsLinks)
 })
 
 btnClose.addEventListener('click', () => {
@@ -17,3 +27,16 @@ btnClose.addEventListener('click', () => {
     newMainScreen.classList.remove('entry_ligth_configs')
 })
 
+function StartLightBox(textTitle, spanTextTitle, spanStyleColor, descriptionContent, linkName, linkhref, imgsLinks) {
+    h1_1.textContent = textTitle
+    h1_1_span.textContent = spanTextTitle
+    h1_1_span.style.color = spanStyleColor
+    description_p.textContent = descriptionContent
+    const link = document.createElement('a');
+    link.textContent = linkName
+    link.href = linkhref
+    description_p.textContent += link.textContent;
+    for (var i = 0; i < 3; i++) {
+        imgsCarousselBootstrap[i].src = imgsLinks[i];
+    }
+}
