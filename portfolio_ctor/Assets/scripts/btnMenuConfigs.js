9159877d91b33f3e4ccf8fea2687d882869efe72
Sami Daniel <samidaniel125@gmail.com>
2023-10-19 20:28:06 -0300

    Configs, and bugs fixxeds about menu

--- portfolio_ctor/Assets/scripts/btnMenuConfigs.js
+++ portfolio_ctor/Assets/scripts/btnMenuConfigs.js
@@ -1,19 +1,19 @@
 const menu = document.querySelector('._navmenu')
 const ul_selectorAll = document.querySelectorAll('.ul_selector a')
 const brigthnessSelector = document.querySelector(`input[type='range']`)
+
 document.querySelector('.css-i6dzq1').addEventListener('click', () => {
-    menu.style.left = '0%'
-    document.body.style.overflow = 'hidden';
+    menu.style.transform = 'translateX(0%)'
+    document.body.style.overflowY = 'hidden';
     ul_selectorAll.forEach(e => {
         e.addEventListener('click', () => {
-            menu.style.left = '100%'
-            document.body.style.overflow = 'visible';
+            e.style.display = 'none'
+            document.body.style.overflowY = 'visible';
         })
     })
+
     brigthnessSelector.addEventListener('change', () => {
-        menu.style.left = '100%'
-        document.body.style.overflow = 'visible'
+        menu.style.transform = 'translateX(100%)'
+        document.body.style.overflowY = 'visible'
     })
-
-
 })
--- portfolio_ctor/Assets/scripts/lightBox.js
+++ portfolio_ctor/Assets/scripts/lightBox.js
@@ -5,41 +5,47 @@ const body = document.querySelector('body')
 const btnClose = document.querySelector('.button_close')
 const imgsCarousselBootstrap = document.querySelectorAll('.img1BoststrappCarousel , .img2BoststrappCarousel , .img3BoststrappCarousel')
 const starterPortfolio = document.querySelector('.starter_lgtBox_port')
+const mediaQueryCell = window.matchMedia('(max-width: 534px')
 let h1_1 = document.querySelector('.titleProject_Ligther')
 let h1_1_span = document.querySelector('.lighter_span_la')
 let description_p = document.querySelector('._lightbox>div:last-child p')
 let aTag = document.getElementById('tagA_lightbox')
 
+
 starterHardSkill.addEventListener('click', () => {
-    StartLightBox()
-    const imgsLinks = [
-        'Assets/imgs/imgs/start_screen.png',
-        'Assets/imgs/imgs/win_screen.png',
-        'Assets/imgs/imgs/lose_screen.png'
-    ]
-    let title = 'BLACK JACK'
-    let spanTextTitle = 'C# .NET FRAMEWORK'
-    let spanStyleColor = 'rgb(227, 1, 227)'
-    let descriptionContent = 'O jogo de 21, também conhecido como blackjack, é um dos jogos de cartas mais populares em cassinos e' +
-        'também pode ser jogado em casa ou online. O objetivo do jogo é ter uma mão com um valor total de 21 pontos, ou o mais próximo possível deste número, sem ultrapassá-lo.' +
-        'Mais em '
-    let linkName = '@gihub.com/sami-daniel'
-    let linkhref = 'https://github.com/sami-daniel?tab=repositories&q=&type=&language=c%23&sort='
-    ConfigLightBox(title, spanTextTitle, spanStyleColor, descriptionContent, linkName, linkhref, imgsLinks)
+    if (!mediaQueryCell.matches) {
+        StartLightBox()
+        const imgsLinks = [
+            'Assets/imgs/imgs/start_screen.png',
+            'Assets/imgs/imgs/win_screen.png',
+            'Assets/imgs/imgs/lose_screen.png'
+        ]
+        let title = 'BLACK JACK'
+        let spanTextTitle = 'C# .NET FRAMEWORK'
+        let spanStyleColor = 'rgb(227, 1, 227)'
+        let descriptionContent = 'O jogo de 21, também conhecido como blackjack, é um dos jogos de cartas mais populares em cassinos e' +
+            'também pode ser jogado em casa ou online. O objetivo do jogo é ter uma mão com um valor total de 21 pontos, ou o mais próximo possível deste número, sem ultrapassá-lo.' +
+            'Mais em '
+        let linkName = '@gihub.com/sami-daniel'
+        let linkhref = 'https://github.com/sami-daniel?tab=repositories&q=&type=&language=c%23&sort='
+        ConfigLightBox(title, spanTextTitle, spanStyleColor, descriptionContent, linkName, linkhref, imgsLinks)
+    }
 })
 
 starterPortfolio.addEventListener('click', () => {
-    StartLightBox()
-    const imgsLinks = [
-        'Assets/imgs/imgs/chimper_lading_page.png',
-        'Assets/imgs/imgs/Smene_photo.png',
-        'Assets/imgs/imgs/mosaico_photo.png'
-    ]
-    let title = 'PROJETOS FRONT-END'
-    let spanTextTitle = 'HTML & JAVASCRIPT'
-    let spanStyleColor = 'rgb(227, 0, 227)'
-    let descriptionContent = 'Aqui estão alguns projetos de desenvolvimento web que fiz durante minha etapa de aprendizado. São meramente ilustrativos e não têm fins lucrativos, porém representam outros sites que também são significativos. Embora não seja meu foco principal, os frameworks e técnicas de desenvolvimento web são muito importantes para o crescimento profissional de um "Desenvolvedor".'
-    ConfigLightBox(title, spanTextTitle, spanStyleColor, descriptionContent, "@github.com/sami-daniel", "https://github.com/sami-daniel?tab=repositories&q=&type=&language=html&sort=", imgsLinks)
+    if (!mediaQueryCell.matches) {
+        StartLightBox()
+        const imgsLinks = [
+            'Assets/imgs/imgs/chimper_lading_page.png',
+            'Assets/imgs/imgs/Smene_photo.png',
+            'Assets/imgs/imgs/mosaico_photo.png'
+        ]
+        let title = 'PROJETOS FRONT-END'
+        let spanTextTitle = 'HTML & JAVASCRIPT'
+        let spanStyleColor = 'rgb(227, 0, 227)'
+        let descriptionContent = 'Aqui estão alguns projetos de desenvolvimento web que fiz durante minha etapa de aprendizado. São meramente ilustrativos e não têm fins lucrativos, porém representam outros sites que também são significativos. Embora não seja meu foco principal, os frameworks e técnicas de desenvolvimento web são muito importantes para o crescimento profissional de um "Desenvolvedor".'
+        ConfigLightBox(title, spanTextTitle, spanStyleColor, descriptionContent, "@github.com/sami-daniel", "https://github.com/sami-daniel?tab=repositories&q=&type=&language=html&sort=", imgsLinks)
+    }
 })
 btnClose.addEventListener('click', () => {
     overlay.classList.remove('entry_overlay')
