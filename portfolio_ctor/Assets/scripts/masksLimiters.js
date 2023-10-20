35c5b4044a27a7ab30f16d253fe58dd05a41dd70
Sami Daniel <samidaniel125@gmail.com>
2023-10-19 21:15:49 -0300

    Masks added

--- portfolio_ctor/Assets/scripts/masksLimiters.js
+++ portfolio_ctor/Assets/scripts/masksLimiters.js
@@ -17,13 +17,31 @@ document.getElementById('insertNumber').addEventListener('input', () => {
     }
 });
 
+let lastName = "";
 
 document.getElementById('insertName').addEventListener('input', () => {
     let input = document.getElementById('insertName');
-    let value = input.value;
+    let value = input.value.trim(); // Remove espaços em branco no início e no fim
 
-    if (value.length > 0) {
-        input.value = value.charAt(0).toUpperCase() + value.slice(1);
+    if (value !== lastName) {
+        lastName = value;
+
+        // Dividir a entrada em palavras separadas por espaço
+        let words = value.split(' ');
+
+        // Capitalizar a primeira letra de cada palavra
+        let capitalizedWords = words.map(word => {
+            if (word.length > 0) {
+                return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+            }
+            return word;
+        });
+
+        // Junte as palavras novamente com espaços
+        let formattedName = capitalizedWords.join(' ');
+
+        // Atualize o valor do campo de entrada com o nome formatado
+        input.value = formattedName;
     }
 });
 
