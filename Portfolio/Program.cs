28540a98df3968407d0fe4643083e736e0ee6e6f
sami daniel <samidaniel125@gmail.com>
2024-05-09 12:28:23 -0300

    adicionando servicos as razor pages

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -5,8 +5,10 @@ namespace Portfolio
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            builder.Services.AddRazorPages();
 
+            var app = builder.Build();
+            
             app.MapGet("/", () => "Hello World!");
 
             app.Run();
